4fb6e1c6c72d257d08b884208df54a4b478f1d82
Stuart Byrne <strtbyrne@gmail.com>
2014-07-16 10:01:09 +0100

    commit_work_6

--- www/js/app.js
+++ www/js/app.js
@@ -8,12 +8,14 @@
 angular.module('magazine', ['ionic', 'magazine.controllers', 'magServices'])
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
+  $ionicPlatform.ready(function($timeout) {
     if(window.StatusBar) {
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
-        Splashscreen.hide();
-    }
+    };
+      $timeout(function(){
+          navigator.splashscreen.hide();
+      }, 4000);
   });
 })
 
